62a4c4a71567f928d40d16858e787e1a26faabf1
Debashish Raut <debashish.adcs@gmail.com>
2025-09-07 19:44:11 +0530

    Initial Commit

---
+++ AwesomeProject/App.jsx
@@ -0,0 +1,32 @@
+import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import First from "./src/First.jsx"
+
+const App = () => {
+  const [name, setName] = useState('Debashish'); // Initial name is 'Piyush'
+
+  const handlePress = () => {
+    setName((prevName) => (prevName === 'Debashish' ? 'Rajeeb' : 'Debashish'));
+  };
+
+  return (
+    <View style={{ flex: 1, backgroundColor: 'black', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ fontSize: 20, textAlign: 'center', marginBottom: 20, color: 'white' }}>{name}</Text>
+      {/* <FunctionBasedComponent /> */}
+      <First />
+      <First />
+      <First />
+      <Button title="Toggle Name" onPress={handlePress} />
+    </View>
+  );
+};
+
+// const FunctionBasedComponent = () => {
+//   return (
+//     <View>
+//       <Text style={{ fontSize: 20, color: 'white' }}>Function Based Text</Text>
+//     </View>
+//   );
+// };
+
+export default App;
---
+++ AwesomeProject/src/First.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+
+export default function First() {
+  return (
+    <>
+        <View>
+            <Text style={{fontSize: 15, color: "white"}}>Hello Bud!!!</Text>
+        </View>
+    </>
+  )
+}
